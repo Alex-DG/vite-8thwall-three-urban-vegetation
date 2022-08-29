517ed2b588475dceece280deb075ce13d2d7a873
Alex Di Guida <alexandre.diguida@gmail.com>
2022-08-29 10:05:01 +0200

    Create multiple butterflies following random values

--- js/Experience/Butterfly.js
+++ js/Experience/Butterfly.js
@@ -1,37 +1,21 @@
 import { FBM } from 'three-noise'
 import { ease, lerp } from './Utils/Maths'
 
-const INSTANCES = 6
+import * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils'
 
 class Butterfly {
   constructor(options) {
     this.scene = options.scene
+    this.instances = options.instances || 3
     this.gltfLoader = options.gltfLoader
 
     this.parentGroup = new THREE.Group()
-    this.parentGroup.position.x = -0.5
-    this.parentGroup.position.z = -0.5
-    this.parentGroup.position.y = 1
-
-    this.group = new THREE.Group()
-
-    this.group.scale.multiplyScalar(0.5)
-
-    this.parentGroup.add(this.group)
     this.scene.add(this.parentGroup)
 
+    this.mixers = []
     this.butterflies = []
 
     this.path = null
-
-    // this.path = {
-    //   start: {
-    //     x: this.parentGroup.position.x,
-    //     y: this.parentGroup.position.y,
-    //     z: this.parentGroup.position.z,
-    //   },
-    //   end: { x: 0.5, y: 1, z: 0.5 },
-    // }
 
     this.clock = new THREE.Clock()
     this.elapsedTime = 0
@@ -50,48 +34,7 @@ class Butterfly {
     await this.setButterfly()
   }
 
-  async setButterfly() {
-    try {
-      const model = await this.gltfLoader.loadAsync(
-        '../../assets/models/flying_butterfly.glb'
-      )
-
-      model.scene.traverse((child) => {
-        if (child instanceof THREE.Mesh) {
-          const material = child.material
-          const map = material.map
-          map.encoding = THREE.sRGBEncoding
-          material.color.setHex(Math.random() * 0xffffff)
-        }
-      })
-
-      this.mixer = new THREE.AnimationMixer(model.scene)
-      this.action = this.mixer.clipAction(model.animations[0])
-      this.action.play()
-
-      let actions = []
-      for (let i = 0; i <= model.animations.length - 1; i++) {
-        actions.push(this.mixer.clipAction(model.animations[i]))
-      }
-
-      this.fbm = new FBM({ seed: Math.random() })
-      const offset = Math.random() * 100
-
-      for (const key in actions) {
-        actions[key].setEffectiveTimeScale(6)
-        setTimeout(() => {
-          actions[key].play()
-        }, Math.random() * 1000)
-      }
-
-      this.group.rotation.y = offset
-      this.group.add(model.scene)
-
-      this.isReady = true
-    } catch (error) {
-      console.error({ error })
-    }
-  }
+  ////////////////////////////////////////////////////////////////////////
 
   attachModel(model) {
     const box = model.geometry.boundingBox
@@ -104,7 +47,81 @@ class Butterfly {
       },
       end: { x: box.max.x, y: 1, z: model.position.z + 0.5 },
     }
+
+    for (let i = 0; i < this.instances; i++) this.createButterfly()
   }
+
+  playAnimation(mixer) {
+    let actions = []
+    for (let i = 0; i <= this.animations.length - 1; i++) {
+      actions.push(mixer.clipAction(this.animations[i]))
+    }
+    for (const key in actions) {
+      actions[key].setEffectiveTimeScale(6)
+      setTimeout(() => {
+        actions[key].play()
+      }, Math.random() * 1000)
+    }
+
+    this.mixers.push(mixer)
+  }
+
+  createButterfly(index) {
+    const butterflyClone = SkeletonUtils.clone(this.butterfly)
+    butterflyClone.name = `butterfly-${index}`
+    butterflyClone.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        const mat = child.material.clone()
+        mat.color.setHex(Math.random() * 0xffffff)
+        mat.emissive = new THREE.Color(Math.random() * 0xffffff)
+        mat.emissiveIntensity = Math.random()
+        mat.emissiveMap = mat.map
+        child.material = mat
+      }
+    })
+    butterflyClone.position.y = 2 * Math.random()
+    butterflyClone.position.x = 2 * Math.random()
+
+    const group = new THREE.Group()
+    group.scale.multiplyScalar(Math.random() + 0.1)
+    group.rotation.y = 100 * Math.random()
+    group.add(butterflyClone)
+
+    const mixer = new THREE.AnimationMixer(butterflyClone)
+    this.playAnimation(mixer)
+
+    this.butterflies.push({
+      butterfly: group,
+      fbm: new FBM({ seed: Math.random() }),
+    })
+    this.parentGroup.add(group)
+  }
+
+  ////////////////////////////////////////////////////////////////////////
+
+  async setButterfly() {
+    try {
+      const model = await this.gltfLoader.loadAsync(
+        '../../assets/models/flying_butterfly.glb'
+      )
+
+      this.butterfly = model.scene
+      this.animations = model.animations
+      this.butterfly.traverse((child) => {
+        if (child instanceof THREE.Mesh) {
+          const material = child.material
+          const map = material.map
+          map.encoding = THREE.sRGBEncoding
+          material.color.convertSRGBToLinear()
+        }
+      })
+      this.isReady = true
+    } catch (error) {
+      console.error({ error })
+    }
+  }
+
+  ////////////////////////////////////////////////////////////////////////
 
   updateFlyingPath() {
     if (this.path) {
@@ -121,22 +138,38 @@ class Butterfly {
     }
   }
 
-  update() {
-    if (!this.isReady) return
+  updatePosition() {
+    this.vec.set(this.clock.elapsedTime, this.clock.elapsedTime)
+    this.butterflies.forEach(({ butterfly, fbm }) => {
+      if (butterfly) {
+        butterfly.position.set(0, fbm.get2(this.vec), 0)
+        butterfly.rotation.y -= this.deltaTime
+      }
+    })
+  }
 
-    // this.t += 0.01
+  updateMixers() {
+    this.mixers.forEach((m) => {
+      m?.update(this.deltaTime)
+    })
+  }
 
+  updateTime() {
     this.elapsedTime = this.clock.getElapsedTime()
     this.deltaTime = this.elapsedTime - this.previousTime
     this.previousTime = this.elapsedTime
-    this.mixer?.update(this.deltaTime)
+  }
 
-    this.vec.set(this.clock.elapsedTime, this.clock.elapsedTime)
-    this.group.position.set(0, this.fbm.get2(this.vec), 0)
-    this.group.rotation.y -= this.deltaTime
+  update() {
+    if (!this.isReady) return
 
+    this.updateTime()
+    this.updateMixers()
+    this.updatePosition()
     this.updateFlyingPath()
 
+    // Rotation animation:
+    // this.t += 0.01
     // this.parentGroup.rotation.y += 0.03
     // this.parentGroup.position.x = 1 * Math.cos(this.t) + 0
     // this.parentGroup.position.z = 1 * Math.sin(this.t) + 0 // These to strings make it work
